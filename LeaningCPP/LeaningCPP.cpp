ea47b790608c92783872453b196729bab6b80b19
Kai <Kaiboucher2007@gmail.com>
2025-10-01 10:33:33 +0100

    updates

--- LeaningCPP/LeaningCPP.cpp
+++ LeaningCPP/LeaningCPP.cpp
@@ -5,14 +5,14 @@
 #include <string>
 using namespace std;
 
-// Defualt Vars
-string Name = "Kai";
-string courseName = "Game Dev";
-string studentID = "B014852p";
-
 // Returns Course Info
 int courseInfo()
 {
+    // Defualt Vars
+    string Name = "Kai";
+    string courseName = "Game Dev";
+    string studentID = "B014852p";
+    
 	std::cout << "Name: " + Name + "\nCourse: " + courseName + "\nStudentID: " + studentID + "\n";
 	return 0;
 }
@@ -41,27 +41,35 @@ int drawBoat()
 	return 0;
 }
 
+int drawMenu()
+{
+    string functions[3] = {"drawBoat", "drawTriangle", "courseInfo"};
+    
+    std::cout << "_________________________\n";
+    std::cout << "| 1        Boat         |\n";
+    std::cout << "| 2      Triangle       |\n";
+    std::cout << "| 3     Course Info     |\n";
+    std::cout << "_________________________\n";
+    
+    string option;
+    cout << "\nSelect an option: ";
+    
+    getline(cin, option);
+
+    int optionInt = stoi(option);
+    
+    string functionToRun = functions[optionInt];
+    
+    functionToRun();
+    
+    return 0;
+}
 
 int main()
 {
     std::cout << "Welcome to Challenges\n\n";
 
-	std::cout << "2 + 2" << " = " << "4\n\n";
-
-	courseInfo();
-
-	drawTriangle();
-
-	drawBoat();
+    drawMenu();
+    
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
