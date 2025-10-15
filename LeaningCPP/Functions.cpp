b662a277e5ecd842c0e7709417192b340e3aab76
Kai <Kaiboucher2007@gmail.com>
2025-10-15 10:00:09 +0100

    help please

---
+++ LeaningCPP/Functions.cpp
@@ -0,0 +1,26 @@
+//
+//  Functions.cpp
+//  LeaningCPP
+//
+//  Created by Kai on 15/10/2025.
+//
+#include <iostream>
+
+
+//int Sqare(int number);
+
+int Sqare(int number)
+{
+    int sqareNumber = number * number;
+    
+    std::cout << sqareNumber;
+    
+    return sqareNumber;
+}
+
+int main()
+{
+    Sqare(4);
+}
+
+
---
+++ LeaningCPP/Functions.hpp
@@ -0,0 +1,13 @@
+//
+//  Functions.hpp
+//  LeaningCPP
+//
+//  Created by Kai on 15/10/2025.
+//
+
+#ifndef Functions_hpp
+#define Functions_hpp
+
+#include <stdio.h>
+
+#endif /* Functions_hpp */
